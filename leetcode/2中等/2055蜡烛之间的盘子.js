c89fb3edee1d7c5598779e1bd367fffac56c8834
huangxiangkun <1845194320@qq.com>
2022-03-08 22:49:07 +0800

    leetcode

--- leetcode/2中等/2055蜡烛之间的盘子.js
+++ leetcode/2中等/2055蜡烛之间的盘子.js
@@ -3,18 +3,44 @@
  * @param {number[][]} queries
  * @return {number[]}
  */
- var platesBetweenCandles = function(s, queries) {
-    let n = queries.length
-    queries.forEach(e=>{
-        let str = s.slice(e[0],e[1])
-        
-    })
-
-    return s.slice(2,3)
+var platesBetweenCandles = function (s, queries) {
+  let n = s.length;
+  let plates = new Array(n).fill(0);
+  for (let i = 0, platesSum = 0; i < n; i++) {
+    if (s[i] === "*") {
+      platesSum++;
+    }
+    plates[i] = platesSum;
+  }
+  let left = new Array(n).fill(-1);
+  for (let i = 0, l = -1; i < n; i++) {
+    if (s[i] === "|") {
+      l = i;
+    }
+    left[i] = l;
+  }
+  let right = new Array(n).fill(-1);
+  for (let i = n - 1, r = -1; i >= 0; i--) {
+    if (s[i] === "|") {
+      r = i;
+    }
+    right[i] = r;
+  }
+  let res = new Array(queries.length).fill(0);
+  for (let i = 0; i < queries.length; i++) {
+    let querie = queries[i];
+    let x = right[querie[0]];
+    let y = left[querie[1]];
+    res[i] = x === -1 || y === -1 || x >= y ? 0 : plates[y] - plates[x];
+  }
+  return res;
 };
-
-console.log(platesBetweenCandles("**|**|***|",[[2,5],[5,9]]));
-
+console.log(
+  platesBetweenCandles("**|||**|**|**", [
+    [2, 5],
+    [5, 9],
+  ])
+);
 
 // 2055. 蜡烛之间的盘子
 // 给你一个长桌子，桌子上盘子和蜡烛排成一列。给你一个下标从 0 开始的字符串 s ，它只包含字符 '*' 和 '|' ，其中 '*' 表示一个 盘子 ，'|' 表示一支 蜡烛 。
@@ -22,7 +48,7 @@ console.log(platesBetweenCandles("**|**|***|",[[2,5],[5,9]]));
 // 同时给你一个下标从 0 开始的二维整数数组 queries ，其中 queries[i] = [lefti, righti] 表示 子字符串 s[lefti...righti] （包含左右端点的字符）。对于每个查询，你需要找到 子字符串中 在 两支蜡烛之间 的盘子的 数目 。如果一个盘子在 子字符串中 左边和右边 都 至少有一支蜡烛，那么这个盘子满足在 两支蜡烛之间 。
 
 // 比方说，s = "||**||**|*" ，查询 [3, 8] ，表示的是子字符串 "*||**|" 。子字符串中在两支蜡烛之间的盘子数目为 2 ，子字符串中右边两个盘子在它们左边和右边 都 至少有一支蜡烛。
-// 请你返回一个整数数组 answer ，其中 answer[i] 是第 i 个查询的答案。
+// 请你返回一个整数数组 reswer ，其中 reswer[i] 是第 i 个查询的答案。
 
 // 示例 1:
 
@@ -42,7 +68,6 @@ console.log(platesBetweenCandles("**|**|***|",[[2,5],[5,9]]));
 // 解释：
 // - queries[0] 有 9 个盘子在蜡烛之间。
 // - 另一个查询没有盘子在蜡烛之间。
- 
 
 // 提示：
 
